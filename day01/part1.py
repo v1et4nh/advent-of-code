79220f06f36578114a1098200f17af00681dcb9c
Viet Anh <v1et4nh@googlemail.com>
2023-12-02 06:38:17 +0100

    added day1

---
+++ day01/part1.py
@@ -0,0 +1,16 @@
+import re
+
+
+def get_first_and_last_digit(str_line):
+    list_num = list(map(int, re.findall(r'\d', str_line)))
+    str_num = str(list_num[0]) + str(list_num[-1])
+    return int(str_num)
+
+
+if __name__ == '__main__':
+    puzzle_input = open('puzzle_input.txt', 'r')
+    lines = puzzle_input.readlines()
+    total_sum = 0
+    for line in lines:
+        total_sum += get_first_and_last_digit(line)
+    print(total_sum)
