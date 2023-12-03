d0e7542c065c4758188c61e0110fe518f366cdf4
Viet Anh <v1et4nh@googlemail.com>
2023-12-03 08:34:11 +0100

    added day04 of 2022

--- 2022/day04/part1.py
+++ 2022/day04/part1.py
@@ -1,3 +1,17 @@
+def is_overlapped(pairs):
+    first, second = pairs.split(',')
+    first  = list(range(int(first.split('-')[0]), int(first.split('-')[1])+1))
+    second = list(range(int(second.split('-')[0]), int(second.split('-')[1])+1))
+    if all(item in second for item in first) or all(item in first for item in second):
+        return 1
+    else:
+        return 0
+
+
 if __name__ == '__main__':
     puzzle_input = open('puzzle_input.txt', 'r')
     lines = puzzle_input.readlines()
+    total_sum = 0
+    for line in lines:
+        total_sum += is_overlapped(line)
+    print(total_sum)
