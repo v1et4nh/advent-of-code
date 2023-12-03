1855c648c4af621777a61d9763c546fe5512fcd7
Viet Anh <v1et4nh@googlemail.com>
2023-12-03 07:32:47 +0100

    added day03 of 2022

--- 2022/day03/part1.py
+++ 2022/day03/part1.py
@@ -1,3 +1,21 @@
+def get_priority(rucksack):
+    # if len(rucksack) % 2 == 0:
+    first  = rucksack[:len(rucksack)//2]
+    second = rucksack[len(rucksack)//2:]
+    common_letter = list(set(first) & set(second))[0]
+    if ord(common_letter) < 91:
+        prio = ord(common_letter) - ord('A') + 27
+    else:
+        prio = ord(common_letter) - ord('a') + 1
+
+    return prio
+
+
 if __name__ == '__main__':
     puzzle_input = open('puzzle_input.txt', 'r')
     lines = puzzle_input.readlines()
+    total_sum = 0
+    for line in lines:
+        priority = get_priority(line)
+        total_sum += priority
+    print(total_sum)
