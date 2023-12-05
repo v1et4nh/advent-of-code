7f24bda0a3171a9e6af96affadd15b3cfc231ef7
Viet Anh <v1et4nh@googlemail.com>
2023-12-05 18:44:22 +0100

    added day06 of 2022

---
+++ 2022/day06/part2.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    puzzle_input = open('puzzle_input.txt', 'r')
+    lines = puzzle_input.readlines()
+    line = [line for line in lines][0]
+    len_distinct_char = 14
+    for i in range(len(line)):
+        tmp_marker = line[i:i+len_distinct_char]
+        if len(set(tmp_marker)) == len_distinct_char:
+            print(i+len_distinct_char)
+            break
