c162e9a2dbf41efaad02f9213cb2f46d6c415241
Viet Anh <v1et4nh@googlemail.com>
2023-12-07 07:34:18 +0100

    added day07

--- 2023/day07/part1.py
+++ 2023/day07/part1.py
@@ -1,3 +1,37 @@
 if __name__ == '__main__':
     puzzle_input = open('puzzle_input.txt', 'r')
     lines = puzzle_input.readlines()
+    lines = [line.replace('\n', '') for line in lines]
+    max_rank = len(lines)
+    cards = []
+    bids  = []
+    type_arr = []
+    for line in lines:
+        card, bid = line.split(' ')
+        cards.append(card.replace('A', 'Z').replace('K', 'Y').replace('Q', 'X').replace('J', 'W'))
+        bids.append(int(bid))
+        if len(set(card)) == 1:  # Five of a kind
+            type_arr.append(7)
+        elif len(set(card)) == 2:
+            if any([True for c in card if card.count(c) == 4]):
+                type_arr.append(6)  # Four of a kind
+            else:
+                type_arr.append(5)  # Full House
+        elif len(set(card)) == 3:
+            if any([True for c in card if card.count(c) == 3]):
+                type_arr.append(4)  # three of a kind
+            else:
+                type_arr.append(3)  # two pair
+        elif len(set(card)) == 4:
+            type_arr.append(2)      # one pair
+        elif len(set(card)) == 5:
+            type_arr.append(1)      # high card
+
+    sorted_cards = [card for card, _, _ in sorted(zip(cards, bids, type_arr), key=lambda pair: (pair[2], pair[0]))]
+    sorted_bids  = [bid for _, bid, _ in sorted(zip(cards, bids, type_arr), key=lambda pair: (pair[2], pair[0]))]
+    sorted_hands = [hand for _, _, hand in sorted(zip(cards, bids, type_arr), key=lambda pair: (pair[2], pair[0]))]
+
+    total_sum = 0
+    for i, bid in enumerate(sorted_bids):
+        total_sum += (i+1) * bid
+    print(total_sum)
---
+++ 2023/day07/part2.py
@@ -0,0 +1,52 @@
+if __name__ == '__main__':
+    puzzle_input = open('puzzle_input.txt', 'r')
+    lines = puzzle_input.readlines()
+    lines = [line.replace('\n', '') for line in lines]
+    max_rank = len(lines)
+    cards = []
+    bids  = []
+    type_arr = []
+    for line in lines:
+        card, bid = line.split(' ')
+        cards.append(card.replace('A', 'Z').replace('K', 'Y').replace('Q', 'X').replace('J', '0'))
+        bids.append(int(bid))
+        if len(set(card)) == 1:  # Five of a kind
+            type_arr.append(7)
+        elif len(set(card)) == 2:
+            if 'J' in card:
+                type_arr.append(7)  # Five of a kind JOKER
+            elif any([True for c in card if card.count(c) == 4]):
+                type_arr.append(6)  # Four of a kind
+            else:
+                type_arr.append(5)  # Full House
+        elif len(set(card)) == 3:
+            if card.count('J') >= 2:
+                type_arr.append(6)  # Four of a kind JOKER
+            elif card.count('J') == 1:
+                if any([True for c in card if card.count(c) == 3]):
+                    type_arr.append(6)
+                else:
+                    type_arr.append(5)
+            elif any([True for c in card if card.count(c) == 3]):
+                type_arr.append(4)  # three of a kind
+            else:
+                type_arr.append(3)  # two pair
+        elif len(set(card)) == 4:
+            if 'J' in card:
+                type_arr.append(4)  # three of a kind JOKER
+            else:
+                type_arr.append(2)      # one pair
+        elif len(set(card)) == 5:
+            if 'J' in card:
+                type_arr.append(2)
+            else:
+                type_arr.append(1)      # high card
+
+    sorted_cards = [card for card, _, _ in sorted(zip(cards, bids, type_arr), key=lambda pair: (pair[2], pair[0]))]
+    sorted_bids  = [bid for _, bid, _ in sorted(zip(cards, bids, type_arr), key=lambda pair: (pair[2], pair[0]))]
+    sorted_hands = [hand for _, _, hand in sorted(zip(cards, bids, type_arr), key=lambda pair: (pair[2], pair[0]))]
+    sorted_total = [[card, hand] for card, _, hand in sorted(zip(cards, bids, type_arr), key=lambda pair: (pair[2], pair[0]))]
+    total_sum = 0
+    for i, bid in enumerate(sorted_bids):
+        total_sum += (i+1) * bid
+    print(total_sum)
