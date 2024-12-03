e3b2cf5a5d039554a9829df863691d386d6bb100
Viet Anh <v1et4nh@googlemail.com>
2024-12-03 07:37:26 +0100

    added day 3 (2024)

---
+++ 2024/day03/part2.py
@@ -0,0 +1,13 @@
+import re
+
+if __name__ == '__main__':
+    puzzle_input = open('puzzle_input.txt', 'r')
+    lines = puzzle_input.readlines()
+    text  = ''.join(lines)
+
+    mul_list = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", text)
+
+    total = 0
+    for m in mul_list:
+        total += int(m[0]) * int(m[1])
+    print(total)
